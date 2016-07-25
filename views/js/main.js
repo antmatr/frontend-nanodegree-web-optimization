f2982166b46d4e7e260b21fb62f7c8b502bfa053
Anton Matrosov <antmatr@yandex.ru>
2016-07-25 20:00:36 +0300

    refactor: Remove Forced Synchronous Layout in JS

--- views/js/main.js
+++ views/js/main.js
@@ -449,9 +449,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      var offsetWidth = document.querySelector(".randomPizzaContainer").offsetWidth;
+      var dx = determineDx(document.querySelector(".randomPizzaContainer"), size);
+      for (var i = 0, l = document.querySelectorAll(".randomPizzaContainer").length; i < l; i++) {
+      var newwidth = (offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
@@ -502,8 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var documentSctollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+      var phase = Math.sin((documentSctollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
